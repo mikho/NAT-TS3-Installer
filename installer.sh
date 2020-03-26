5764c1c56e96aa5b08742e818cf27f6653d794f2
mikho <mikho.mail@gmail.com>
2020-03-26 07:55:09 +0100

    After test installs

--- installer.sh
+++ installer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# Easy TeamSpeak 3 installer for Debian based OS
-# Tested by bosscoder on Debian 7/8 and Ubuntu 14.04 LTS
+# Easy TeamSpeak 3 installer for Debian or CentOS based OS
+#
+#
+# 
 
 # Check for root account
 if [[ "$EUID" -ne 0 ]]; then
@@ -8,39 +10,53 @@ if [[ "$EUID" -ne 0 ]]; then
   exit 1
 fi
 
-# Check supported OS
-if [ -e '/etc/redhat-release' ] ; then
-  echo 'Error: Sorry, this installer works only on Debian or Ubuntu'
-  exit 1
-fi
-
-# Get the internal IP of the server
-pvtip=$( ifconfig  | grep 'inet addr:'| grep -v '127.0.0*' | cut -d ':' -f2 | awk '{ print $1}' )
-
-# Get the external public IP of the server
-pubip=$( wget -qO- http://ipinfo.io/ip )
-
-# Ask the user if they accept the teamspeak license
-read -p "Do you accept the Teamspeak License? [y/n]: " licensepermission
+# Ask the user if they accept the teamspeak license. Quit if not accepted
 while true; do
+read -p "Do you accept the Teamspeak License? [y/n]: " licensepermission
   if [[ "$licensepermission" == "y" ]]; then
     echo "You accepted the Teamspeak License."
     echo "You can view the license by using: cat /opt/ts3/LICENSE"
     break
-  elif ! [[ "$licensepermission" = "n" ]]; then
+  elif [[ "$licensepermission" = "n" ]]; then
     echo "Error: You did not accept the Teamspeak License. Quiting installation."
     exit 2
     break
-  else
-    break
   fi
 done
+
+# Check supported OS and install required packages
+echo "Installing required packages"
+if [ -e '/etc/redhat-release' ] ; then
+  yum update -yes
+  yum install wget sudo telnet bzip2 tar ca-certificates -yes 
+elif [ -e '/etc/os-release' ] ; then
+  apt-get update
+  apt-get install -y wget sudo telnet bzip2 tar ca-certificates
+else 
+  echo "Can't figure out what distribution you are running, exiting"
+  exit 2
+  break
+fi
+
+# Check if packages were installed, else exit with error
+CMDS="wget sudo telnet bzip2"
+for i in $CMDS
+do
+	type -P $i &>/dev/null  && continue  || { echo "$i command not found. Computer says No."; exit 1; }
+ done
+
+
+# Get the internal IP of the server
+pvtip=$( ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' )
+
+# Get the external public IP of the server
+pubip=$( wget -qO- http://ipinfo.io/ip )
 
 
 
 # Gives user the internal ip for reference and ask for desired ports
 echo "Your private internal IP is: $pvtip"
-echo "If you are installing this on a NAT VPS, use your assigned ports.
+echo "If you are installing this on a NAT VPS, use your assigned ports."
 read -p "Enter Voice Server port [9987]: " vport
 while true; do
   if [[ "$vport" == "" ]]; then
@@ -90,19 +106,15 @@ fi
 echo "-------------------------------------------------------"
 echo "Detecting latest TeamSpeak 3 version, please wait..."
 echo "-------------------------------------------------------"
-ts3version=$(wget 'https://files.teamspeak-services.com/releases/server/' -q -O -  | grep -Eo '([0-9]{1,}\.)+[0-9]{1,}' | uniq | sort -r -V | head -n 1
+ts3version=$(wget 'https://files.teamspeak-services.com/releases/server/' -q -O - --no-check-certificate | grep -Eo '([0-9]{1,}\.)+[0-9]{1,}' | uniq | sort -r -V | head -n 1)
 echo "Latest version found is: " $ts3version
 
 # Get OS Arch and download correct packages
 if [ "$(arch)" != 'x86_64' ]; then
-    wget "https://files.teamspeak-services.com/releases/server/"$ts3version"/teamspeak3-server_linux_x86-"$ts3version".tar.bz2" -P /opt/ts3/
+    wget "https://files.teamspeak-services.com/releases/server/"$ts3version"/teamspeak3-server_linux_x86-"$ts3version".tar.bz2" --no-check-certificate -P /opt/ts3/
 else
-    wget "https://files.teamspeak-services.com/releases/server/"$ts3version"/teamspeak3-server_linux_amd64-"$ts3version".tar.bz2" -P /opt/ts3/
+    wget "https://files.teamspeak-services.com/releases/server/"$ts3version"/teamspeak3-server_linux_amd64-"$ts3version".tar.bz2" --no-check-certificate -P /opt/ts3/
 fi
-
-# Install required packages
-apt-get update
-apt-get install -y sudo telnet bzip2
 
 # Create non-privileged user for TS3 server, and moves home directory under /etc
 adduser --disabled-login --gecos "ts3server" ts3
@@ -116,40 +128,67 @@ rm -f /opt/ts3/teamspeak3-server_linux*.tar.bz2
 chown -R ts3:ts3 /opt/ts3/
 
 # Create autostart script
-cat > /etc/init.d/teamspeak3 <<"EOF"
-#!/bin/sh
-### BEGIN INIT INFO
-# Provides:          TeamSpeak 3 Server
-# Required-Start:    networking
-# Required-Stop:
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: TeamSpeak 3 Server Daemon
-# Description:       Starts/Stops/Restarts the TeamSpeak 3 Server Daemon
-### END INIT INFO
+# Check if systemd or init
+astart="$(ps --no-headers -o comm 1)"
+if [ $astart == "init" ]; then
+  cat > /etc/init.d/ts3server <<"EOF"
+  #!/bin/sh
+  ### BEGIN INIT INFO
+  # Provides:          TeamSpeak 3 Server
+  # Required-Start:    networking
+  # Required-Stop:
+  # Default-Start:     2 3 4 5
+  # Default-Stop:      0 1 6
+  # Short-Description: TeamSpeak 3 Server Daemon
+  # Description:       Starts/Stops/Restarts the TeamSpeak 3 Server Daemon
+  ### END INIT INFO
 
-set -e
+  set -e
 
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-DESC="TeamSpeak 3 Server"
-NAME=ts3
-USER=ts3
-DIR=/opt/ts3/
-DAEMON=$DIR/ts3server_startscript.sh
-SCRIPTNAME=/etc/init.d/$NAME
+  PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+  DESC="TeamSpeak 3 Server"
+  NAME=ts3
+  USER=ts3
+  DIR=/opt/ts3/
+  DAEMON=$DIR/ts3server_startscript.sh
+  SCRIPTNAME=/etc/init.d/$NAME
 
-test -x $DAEMON || exit 0
+  test -x $DAEMON || exit 0
 
-cd $DIR
-sudo -u ts3 ./ts3server_startscript.sh $1
+  cd $DIR
+  sudo -u ts3 ./ts3server_startscript.sh $1
 EOF
-chmod 755 /etc/init.d/teamspeak3
+chmod 755 /etc/init.d/ts3server
+# Set TS3 server to auto start on system boot
+update-rc.d teamspeak3 defaults
+else # $astart <> "init" 
+cat > /etc/systemd/system/ts3server.service <<"EOF"
+[Unit]
+Description=TeamSpeak 3 Server
+Documentation=http://www.teamspeak.com/?page=literature
+Wants=network.target
+After=syslog.target network.target
+
+[Service]
+User=ts3
+WorkingDirectory=/opt/ts3
+Type=forking
+ExecStart=/opt/ts3/ts3server_startscript.sh start
+ExecStop=/opt/ts3/ts3server_startscript.sh stop
+ExecReload=/opt/ts3/ts3server_startscript.sh reload
+Restart=on-failure
+RestartSec=15
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+ # Set TS3 server to auto start on system boot
+ systemctl enable ts3server
+fi
 
 # Assign right ports and password to TS3 server
 sed -i 's/COMMANDLINE_PARAMETERS=""/COMMANDLINE_PARAMETERS="query_port='$qport' query_ip=0.0.0.0 default_voice_port='$vport' voice_ip=0.0.0.0 filetransfer_port='$fport' filetransfer_ip=0.0.0.0 serveradmin_password='$apass'"/' /opt/ts3/ts3server_startscript.sh
-
-# Set TS3 server to auto start on system boot
-update-rc.d teamspeak3 defaults
 
 # Give user all the information
 echo ""
@@ -162,6 +201,7 @@ echo "Accepting the license..."
 touch /opt/ts3/.ts3server_license_accepted
 echo "Accepted license!"
 echo "Automatically configuring ports..."
+echo ""
 echo "Voice server is available at $pubip:$vport"
 echo ""
 echo "The file transfer port is: $fport"
@@ -170,14 +210,22 @@ echo ""
 read -p "Start the server now? [y/n]: " startopt
 sleep 1
 if [ "$startopt" == "y" ] || [ "$startopt" == "yes" ]; then
-  echo "To find your private token to configure your server, use:"
-  echo "cd opt/ts3/logs"
-  echo "cat $(ls -t1 | tail -1)"
+  if [ $astart == "init" ]; then
+    /etc/init.d/ts3server start
+  else
+   service ts3server start
+  fi   
   sleep 2
-  /etc/init.d/teamspeak3 start
 else
   echo "Run the following command to manually start the server:"
-  echo "/etc/init.d/teamspeak3 start"
+   if [ $astart == "init" ]; then
+    echo "/etc/init.d/ts3server start"
+  else
+   echo "service ts3server start"
+  fi
 fi
+  echo "After first start,"
+  echo "find your private token by running the following command:"
+  echo "cat /opt/ts3/logs/\$(ls -t1 /opt/ts3/logs | tail -1)"
 
 exit 0
